65bce9f138905aa546bc374cc7fb8f86c22fc44b
Jhony de Paula <jhony.paulaa@gmail.com>
2023-05-31 12:39:31 -0300

    add biblioteca material (react-native-papper)

--- mobile/index.js
+++ mobile/index.js
@@ -6,4 +6,12 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
+
 AppRegistry.registerComponent(appName, () => App);
