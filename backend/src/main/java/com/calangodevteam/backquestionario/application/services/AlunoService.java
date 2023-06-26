87f86b14541e3e75cdb826adc2a272cd623c4338
Jhony de Paula <jhony.paulaa@gmail.com>
2023-06-26 11:27:36 -0300

    att back-end
    add verificação se já existe um aluno com esse email

--- backend/src/main/java/com/calangodevteam/backquestionario/application/services/AlunoService.java
+++ backend/src/main/java/com/calangodevteam/backquestionario/application/services/AlunoService.java
@@ -3,6 +3,7 @@ package com.calangodevteam.backquestionario.application.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.calangodevteam.backquestionario.domain.exceptions.BadRequestException;
 import com.calangodevteam.backquestionario.domain.exceptions.ObjectNotFoundException;
 import com.calangodevteam.backquestionario.domain.models.Aluno;
 import com.calangodevteam.backquestionario.domain.repositories.AlunoRepository;
@@ -22,8 +23,13 @@ public class AlunoService {
 		return aluno;
 	}
 
-	public Aluno create(Aluno aluno) {
-		return alunoRepository.save(aluno);
+	public Aluno create(Aluno aluno) {	
+		Boolean alunoExite = alunoRepository.findByEmail(aluno.getEmail()).isPresent();
+	
+		if(alunoExite)
+			throw new BadRequestException("Email em Uso!");
+		else
+			return alunoRepository.save(aluno);
 	}
 
 }
--- backend/src/main/java/com/calangodevteam/backquestionario/domain/repositories/AlunoRepository.java
+++ backend/src/main/java/com/calangodevteam/backquestionario/domain/repositories/AlunoRepository.java
@@ -1,5 +1,7 @@
 package com.calangodevteam.backquestionario.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +11,6 @@ import com.calangodevteam.backquestionario.domain.models.Aluno;
 public interface AlunoRepository extends JpaRepository<Aluno, Integer>{
 
 	Page<Aluno> findAll(Pageable pageable);
+	Optional<Aluno> findByEmail(String email);
 
 }
